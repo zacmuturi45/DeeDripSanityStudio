96ac6a32651e377fafb1a998c9cd16ea23765d58
zacmuturi <zacmuturi45@gmail.com>
2026-06-02 11:16:18 +0300

    Updated Schema

--- schemaTypes/blogPost.ts
+++ schemaTypes/blogPost.ts
@@ -1,89 +1,100 @@
 import {defineType, defineField} from 'sanity'
 
 export const post = defineType({
-    name: "post",
-    title: "Posts",
-    type: "document",
+  name: 'post',
+  title: 'Posts',
+  type: 'document',
 
-    fields: [
-        defineField({
-            name: "title",
-            title: "Title",
-            type: "string",
-            validation: (Rule) => Rule.required(),
-        }),
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
 
-        defineField({
-            name: "slug",
-            title: "Slug",
-            type: "slug",
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
 
-            options: {
-                source: "title",
-            },
-        }),
+      options: {
+        source: 'title',
+      },
+    }),
 
-        defineField({
-            name: "mainImage",
-            title: "Main Image",
-            type: "image",
+    defineField({
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
 
-            options: {
-                hotspot: true,
-            },
-        }),
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+          description: 'Important for SEO and accessibility',
+          options: {
+            isHighlighted: true,
+          },
+        },
+      ],
+    }),
 
-        defineField({
-            name: "excerpt",
-            title: "Excerpt",
-            type: "text",
-            rows: 4,
-        }),
+    defineField({
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+      rows: 4,
+    }),
 
-        defineField({
-            name: "body",
-            title: "Body",
-            type: "array",
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'array',
 
-            of: [
-                {type: "block",},
+      of: [
+        {type: 'block'},
 
-                {
-                    type: "image",
-                    options: {
-                        hotspot: true,
-                    },
-                },
-            ],
-        }),
+        {
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
+    }),
 
-        defineField({
-            name: "author",
-            title: "Author",
-            type: "reference",
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
 
-            to: [{ type: "author"}],
-        }),
+      to: [{type: 'author'}],
+    }),
 
-        defineField({
-            name: "categories",
-            title: "Categories",
-            type: "array",
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
 
-            of: [
-                {
-                    type: "reference",
-                    to: [{ type: "category"}],
-                },
-            ],
-        }),
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'category'}],
+        },
+      ],
+    }),
 
-        defineField({
-            name: "publishedAt",
-            title: "Published At",
-            type: "datetime",
+    defineField({
+      name: 'publishedAt',
+      title: 'Published At',
+      type: 'datetime',
 
-            initialValue: () => new Date().toISOString(),
-        }),
-    ],
-});
+      initialValue: () => new Date().toISOString(),
+    }),
+  ],
+})
